17c94f8a4b9e0aabc0196e852272fb5dd72fa25e
M. Akutagawa <makutaga@icloud.com>
2016-03-02 01:13:35 +0900

    Code is refined

--- pwm_test.py
+++ pwm_test.py
@@ -21,7 +21,7 @@ pinB_a_phase  = pinB_a1
 pinB_b_enable = pinB_b2
 pinB_b_phase  = pinB_b1
 
-pwm_freq = 100
+pwm_freq = 1000
 pwm_limit = 95
 
 class _Getch:
@@ -48,13 +48,13 @@ class _GetchUnix:
         return ch
 
 class PWMMotor:
-	def __init__(self, pin_phase, pin_enable, freq=60):
+	def __init__(self, pin_phase, pin_enable, freq=1000):
 		self.p_phase = pin_phase
 		self.p_enable = pin_enable
 		self.pwm_freq = freq
 		self.pwm_limit = 100
 		self.duty = 0.0
-		self.pwm = GPIO.PWM(self.p_phase, self.pwm_freq)
+		self.pwm = GPIO.PWM(self.p_enable, self.pwm_freq)
 		self.pwm.start(self.duty)
 	def rotate(self, vel):
 		if vel >= 0:
@@ -65,51 +65,21 @@ class PWMMotor:
 		self.duty = vel
 		if (self.duty > self.pwm_limit):
 			self.duty =self.pwm_limit
-		self.pwm.ChangeDutyCycle(dc)
+		self.pwm.ChangeDutyCycle(self.duty)
 
 class Locomotor:
-    def __init__self(self, mt_left, mt_right):
-        self.motor_left = mt_left
-        self.motor_right = mt_right
-
-def motor_rotate(pin_phase, pwm, vel):
-	if vel >= 0:
-		GPIO.output(pin_phase, GPIO.LOW)
-	elif vel < 0:
-		GPIO.output(pin_phase, GPIO.HIGH)
-		vel = -vel
-	dc = vel
-	if (dc > pwm_limit):
-		dc = pwm_limit
-	pwm.ChangeDutyCycle(dc)
-
-def move_forward(phasea, pwma, phaseb, pwmb, vel):
-	motor_rotate(phasea, pwma, vel)
-	motor_rotate(phaseb, pwmb, vel)
-
-def move_back(phasea, pwma, phaseb, pwmb, vel):
-	motor_rotate(phasea, pwma, -vel)
-	motor_rotate(phaseb, pwmb, -vel)
-
-def move_stop(phasea, pwma, phaseb, pwmb):
-	move_forward(phasea, pwma, phaseb, pwmb, 0)
-
-def turn_right(phasea, pwma, phaseb, pwmb, vel):
-	motor_rotate(phasea, pwma, vel)
-	motor_rotate(phaseb, pwmb, -vel)
-
-def turn_left(phasea, pwma, phaseb, pwmb, vel):
-	motor_rotate(phasea, pwma, -vel)
-	motor_rotate(phaseb, pwmb, vel)
-
-def aux_up(phase, pwm, vel):
-	motor_rotate(phase, pwm, vel)
-
-def aux_down(phase, pwm, vel):
-	motor_rotate(phase, pwm, -vel)
-
-def aux_stop(phase, pwm):
-	aux_up(phase, pwm, 0)
+	def __init__(self, mt_left, mt_right):
+		self.motor_left = mt_left
+		self.motor_right = mt_right
+	def move(self, vel_l, vel_r):
+		self.motor_left.rotate(vel_l)
+		self.motor_right.rotate(vel_r)
+	def move_forward(self, vel):
+		self.move(vel, vel)
+	def move_back(self, vel):
+		self.move(-vel, -vel)
+	def stop(self):
+		self.move(0, 0)
 
 if __name__ == "__main__":
 	getch = _Getch()
@@ -117,19 +87,15 @@ if __name__ == "__main__":
 	GPIO.setmode(GPIO.BOARD)
 	GPIO.setup(pins, GPIO.OUT, initial=GPIO.LOW)
 
-#	pwmA_a = GPIO.PWM(pinA_a_enable, pwm_freq)
-#	pwmA_b = GPIO.PWM(pinA_b_enable, pwm_freq)
-#	pwmB_a = GPIO.PWM(pinB_a_enable, pwm_freq)
-#	pwmB_b = GPIO.PWM(pinB_b_enable, pwm_freq)
-#	pwmA_a.start(0)
-#	pwmA_b.start(0)
-#	pwmB_a.start(0)
-#	pwmB_b.start(0)
+	mt_left = PWMMotor(pinA_a_phase, pinA_a_enable, pwm_freq)
+	mt_right = PWMMotor(pinA_b_phase, pinA_b_enable, pwm_freq)
+	mt_dummy = PWMMotor(pinB_a_phase, pinB_a_enable, pwm_freq)
+	mt_arm = PWMMotor(pinB_b_phase, pinB_b_enable, pwm_freq)
 
-	mt_left = PWMMotor(pinA_a_phase, pinA_a_enable)
-	mt_right = PWMMotor(pinA_b_phase, pinA_b_enable)
-	mt_dummy = PWMMotor(pinB_a_phase, pinB_a_enable)
-	mt_arm = PWMMotor(pinB_b_phase, pinB_b_enable)
+	loc = Locomotor(mt_left, mt_right)
+	vel_normal = pwm_limit * 0.5
+	vel_high = pwm_limit * 0.8
+	vel_arm = pwm_limit * 0.3
 
 	try:
 		while 1:
@@ -137,29 +103,44 @@ if __name__ == "__main__":
 			key = getch()
 			print key
 
-			if key == "w":
+			mt_arm.rotate(0)
+			if key == "W":
+				print "Forward (Fast)"
+				loc.move_forward(vel_high)
+			elif key == "w":
 				print "Forward"
-				move_forward(pinA_a_phase, pwmA_a, pinA_b_phase, pwmA_b, pwm_limit)
+				loc.move_forward(vel_normal)
+			elif key == "Z":
+				print "Back (Fast)"
+				loc.move_back(vel_high)
 			elif key == "z":
 				print "Back"
-				move_back(pinA_a_phase, pwmA_a, pinA_b_phase, pwmA_b, pwm_limit)
+				loc.move_back(vel_normal)
+			elif key == "A":
+				print "Left"
+				loc.move(-vel_normal, vel_normal)
 			elif key == "a":
 				print "Left"
-				turn_left(pinA_a_phase, pwmA_a, pinA_b_phase, pwmA_b, pwm_limit)
+				loc.move(0, vel_normal)
+			elif key == "S":
+				print "Right"
+				loc.move(vel_normal, -vel_normal)
 			elif key == "s":
 				print "Right"
-				turn_right(pinA_a_phase, pwmA_a, pinA_b_phase, pwmA_b, pwm_limit)
+				loc.move(vel_normal, 0)
 			elif key == "e":
 				print "Up"
-				aux_up(pinB_b_phase, pwmB_b, pwm_limit * 0.7)
+				loc.stop()
+				mt_arm.rotate(vel_arm)
 			elif key == "x":
 				print "Down"
-				aux_down(pinB_b_phase, pwmB_b, pwm_limit * 0.7)
+				loc.stop()
+				mt_arm.rotate(-vel_arm)
 			elif key == "q":
 				break
 			else:
-				move_stop(pinA_a_phase, pwmA_a, pinA_b_phase, pwmA_b)
-				aux_stop(pinB_b_phase, pwmB_b)
+				loc.stop()
+				mt_arm.rotate(0)
 
 			time.sleep(0.2)
 
