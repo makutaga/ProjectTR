0f80397295ef715f13a9b3d73646cbd256567c0e
M. Akutagawa <makutaga@icloud.com>
2016-03-11 00:45:54 +0900

    super() を適切に訂正．ドキュメンテーション文字列を追加

--- i2c_devices.py
+++ i2c_devices.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Raspberry Pi 2 でI2Cを使うためのクラスの定義
+"""
+
+__author__ = "M. Akutagawa"
 
 import RPi.GPIO as GPIO
 import time
 import smbus
+import math
 
 mag_dev_addr = 0x1e
 
@@ -18,14 +26,35 @@ mag_f_z_l = 0x08
 mag_f_status = 0x09
 
 class I2CDevice(object):
+	"""汎用I2Cデバイスクラス"""
 	def __init__(self, bus, dev_addr):
+		"""
+		コンストラクタ．
+		bus には smbus.SMBus(1) をつかう．
+		dev_addr はデバイスのI2Cアドレス
+		"""
 		self.bus = bus
 		self.dev_addr = dev_addr
+
 	def write_byte(self, reg, dat):
+		"""
+		1バイト書き込み．
+		reg で指定されるレジスタに dat を書き込む．
+		"""
 		self.bus.write_byte_data(self.dev_addr, reg, dat)
+
 	def read_byte(self, reg):
+		"""
+		1バイト読み込み．
+		reg で指定されるレジスタから 1バイト読みこむ．
+		"""
 		return self.bus.read_byte_data(self.dev_addr, reg)
 	def read_word(self, reg1):
+		"""
+		1ワード（2バイト）読み込み．
+		reg で指定されるレジスタから 1ワード（2バイト）読みこむ．
+		符号付き2バイト整数を仮定．
+		"""
 		dat = (self.bus.read_byte_data(self.dev_addr, reg1) << 8) + self.bus.read_byte_data(self.dev_addr, reg1 + 1)
 		if dat > 0x8000:
 			dat = dat - 0xffff
@@ -34,14 +63,14 @@ class I2CDevice(object):
 class MagSensor(I2CDevice):
 	def __init__(self, bus, dev_addr):
 		super(MagSensor, self).__init__(bus, dev_addr)
-		super(MagSensor, self).write_byte(mag_f_cra, 0x70)
-		super(MagSensor, self).write_byte(mag_f_crb, 0xa0)
+		self.write_byte(mag_f_cra, 0x70)
+		self.write_byte(mag_f_crb, 0xa0)
 	def meas_single(self):
-		super(MagSensor, self).write_byte(mag_f_mode, 0x01)
+		self.write_byte(mag_f_mode, 0x01)
 		time.sleep(0.05)
-		bx = super(MagSensor, self).read_word(mag_f_x_h)
-		by = super(MagSensor, self).read_word(mag_f_y_h)
-		bz = super(MagSensor, self).read_word(mag_f_z_h)
+		bx = self.read_word(mag_f_x_h)
+		by = self.read_word(mag_f_y_h)
+		bz = self.read_word(mag_f_z_h)
 		return list([bx, by, bz])
 
 if __name__ == "__main__":
@@ -50,7 +79,10 @@ if __name__ == "__main__":
 
 	while True:
 		try:
-			print mag.meas_single()
+			b = mag.meas_single()
+			bb = math.sqrt(b[0]**2 + b[1]**2 + b[2]**2)
+			
+			print mag.meas_single(), bb
 
 		except KeyboardInterrupt:
 			break
